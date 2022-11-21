001529d1a5e474c78b6d800253715acd7d50879e
Seaborn Mercer <surfermercer@gmail.com>
2022-11-21 09:39:00 -0600

    Phases 2-5

--- Program.cs
+++ Program.cs
@@ -7,10 +7,28 @@ namespace GuessingGame
     {
         static void Main(string[] args)
         {
+            Random r = new Random();
+
+            int secretNum = r.Next(1, 100);
+
             Console.WriteLine("Guess the secret number: ");
-            string userGuess = Console.ReadLine();
-            int parsedGuess = int.Parse(userGuess);
-            Console.WriteLine(parsedGuess);
+            bool foundNum = false;  
+            int userTries = 0;
+            int userTriesRemaining = 4;
+            while((foundNum != true) && (userTriesRemaining != 0)) {
+                string userGuess = Console.ReadLine();
+                int parsedGuess = int.Parse(userGuess);
+                if(parsedGuess == secretNum) {
+                    Console.WriteLine("Yep, thats the number");
+                    foundNum = true;
+                }
+                else {
+                userTries++;
+                Console.WriteLine("Nah that aint it, that was attempt # " + $"{userTries}");
+                userTriesRemaining--;
+                }
+                
+            }
         }
     }
 }
